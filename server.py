8d0a84ddae974a4ad8abee78762a06d700b37673
Brandon Tarrant <brandontarrant04@gmail.com>
2022-02-07 22:53:45 -0500

    Add the create_friend route.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -9,6 +9,17 @@ def index():
     friends = Friend.get_all()
     print(friends)
     return render_template("index.html", all_friends=friends)
+
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form["occ"]
+    }
+    Friend.save(data)
+    return redirect('/')
 
 
 if __name__ == "__main__":
